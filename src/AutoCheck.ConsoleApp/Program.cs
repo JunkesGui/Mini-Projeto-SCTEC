02aec4c7efa60ef88df669e26f291c90ecbedf97
Guilherme Junkes <107532122+JunkesGui@users.noreply.github.com>
2026-08-24 13:57:48 +0000

    cria a base do menu com a func ExibirMenu

---
+++ src/AutoCheck.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+using System.Linq.Expressions;
+using System.Numerics;
+
+void ExibirMenu()
+{   
+    Console.WriteLine("===================================================");
+    Console.WriteLine("Bem vindo ao Sistema de Vistoria Técnica Automotiva");
+    Console.WriteLine("===================================================");
+    Console.WriteLine("Escolha uma opção para continuar: ");
+    Console.WriteLine("1 - Realizar Nova Vistoria");
+    Console.WriteLine("2 - Exibir Relatório de Vistorias");
+    Console.WriteLine("0 - Sair");
+}
+
+int opcao; 
+do
+{
+    ExibirMenu();
+    opcao = Convert.ToInt16(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+        Console.WriteLine("opcao1");
+        break;
+
+        case 2:
+        Console.WriteLine("opcao2");
+        break;
+    }
+} while (opcao != 0);
