70a9b9f2c279deb5b0c3d81f77f62d2707163137
Guilherme Junkes <107532122+JunkesGui@users.noreply.github.com>
2026-08-24 22:36:53 +0000

    refatora a classe `veiculo` e adiciona namespaces

--- src/AutoCheck.ConsoleApp/Models/Veiculo.cs
+++ src/AutoCheck.ConsoleApp/Models/Veiculo.cs
@@ -1,26 +1,29 @@
-abstract class Veiculo{
-    private string Marca;
-    private string Modelo;
-    private int Ano;
-    private int Quilometragem;
-    private List<ItemVistoria> VistoriaRealizada;
+namespace AutoCheckConsole
+{
+    public abstract class Veiculo{
+        protected string Marca {get; set;}
+        protected string Modelo {get; set;}
+        protected int Ano {get; set;}
+        protected int Quilometragem {get; set;}
+        protected List<ItemVistoria> VistoriaRealizada;
 
-    public Veiculo(string Marca, string Modelo, int Ano, int Quilometragem, List<ItemVistoria> VistoriaRealizada)
-    {
-        this.Marca = Marca;
-        this.Modelo = Modelo;
-        this.Ano = Ano;
-        this.Quilometragem = Quilometragem;
-        this.VistoriaRealizada = VistoriaRealizada;
+        public Veiculo(string Marca, string Modelo, int Ano, int Quilometragem, List<ItemVistoria> VistoriaRealizada)
+        {
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+            this.Ano = Ano;
+            this.Quilometragem = Quilometragem;
+            this.VistoriaRealizada = VistoriaRealizada;
+        }
+
+        public void AdicionarItemVistoriado(string nome, string status)
+        {
+            
+        }
+
+        public virtual List<string> ObterChecklistObrigatorio()
+        {
+            return ["Nível de óleo", "Documentação", "Extintor", "Calibração dos Pneus", "Sistema Elétrico"];
+        } 
     }
-
-    public void AdicionarItemVistoriado(string nome, string status)
-    {
-        
-    }
-
-    public virtual List<string> ObterChecklistObrigatorio()
-    {
-        return ["Nível de óleo", "Documentação", "Extintor", "Calibração dos Pneus", "Sistema Elétrico"];
-    } 
 }
--- src/AutoCheck.ConsoleApp/Program.cs
+++ src/AutoCheck.ConsoleApp/Program.cs
@@ -1,30 +1,29 @@
-using System.Linq.Expressions;
-using System.Numerics;
+using AutoCheckConsole;
 
-void ExibirMenu()
-{   
-    Console.WriteLine("===================================================");
-    Console.WriteLine("Bem vindo ao Sistema de Vistoria Técnica Automotiva");
-    Console.WriteLine("===================================================");
-    Console.WriteLine("Escolha uma opção para continuar: ");
-    Console.WriteLine("1 - Realizar Nova Vistoria");
-    Console.WriteLine("2 - Exibir Relatório de Vistorias");
-    Console.WriteLine("0 - Sair");
-}
-
-int opcao; 
-do
-{
-    ExibirMenu();
-    opcao = Convert.ToInt16(Console.ReadLine());
-    switch (opcao)
-    {
-        case 1:
-        Console.WriteLine("opcao1");
-        break;
-
-        case 2:
-        Console.WriteLine("opcao2");
-        break;
+    void ExibirMenu()
+    {   
+        Console.WriteLine("===================================================");
+        Console.WriteLine("Bem vindo ao Sistema de Vistoria Técnica Automotiva");
+        Console.WriteLine("===================================================");
+        Console.WriteLine("Escolha uma opção para continuar: ");
+        Console.WriteLine("1 - Realizar Nova Vistoria");
+        Console.WriteLine("2 - Exibir Relatório de Vistorias");
+        Console.WriteLine("0 - Sair");
     }
-} while (opcao != 0);
+
+    int opcao; 
+    do
+    {
+        ExibirMenu();
+        opcao = Convert.ToInt16(Console.ReadLine());
+        switch (opcao)
+        {
+            case 1:
+            Console.WriteLine("opcao1");
+            break;
+
+            case 2:
+            Console.WriteLine("opcao2");
+            break;
+        }
+    } while (opcao != 0);
